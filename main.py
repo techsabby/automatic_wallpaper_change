4b03ac31d656ea3b7307eff4ef57ca5b1752c140
techsabby <sebastian.login@proton.me>
2023-04-23 22:39:47 -0400

    Testing MVP

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def change_wallpaper():
 	now = datetime.datetime.now()
 
 	if (now.hour >= 12): # it is after noon / set night time wallpaper 
+		print("if statment ran")
 		sunset = data['sunset']
 		sunset_hour = int(sunset[0] + sunset[1]) - 4
 		sunset_hour = sunset_hour + 12 # set hour to be in 24 hour format 
@@ -26,16 +27,20 @@ def change_wallpaper():
 		target_time = datetime.datetime(now.year, now.month, now.day, sunset_hour, sunset_minute)
 
 		while datetime.datetime.now() < target_time:
+			print("waiting for target_time: " + str(target_time))
+			print("the current time is: " + str(datetime.datetime.now()))
 			time.sleep(60)
 
 		ctypes.windll.user32.SystemParametersInfoW(20, 0, "W:\\Backup\\Documents\\Wallpapers\\studioghibli.jpg", 0)
 
 		# wait until midnight to call function again 
-		while datetime.date.now().hour > 0:
+		while datetime.datetime.now().hour > 0:
+			print("waiting for midnight, current hour is: " + str(datetime.datetime.now().hour))
 			time.sleep(1200)
 
-		changewallpaper()
+		change_wallpaper()
 	else: # it is before noon / set day time wallpaper 
+		print("else statement ran")
 		sunrise = data['sunrise']
 		sunrise_hour = int(sunrise[0] + sunrise[1]) - 4
 		sunrise_minute = int(sunrise[3] + sunrise[4])
@@ -44,13 +49,16 @@ def change_wallpaper():
 		target_time = datetime.datetime(now.year, now.month, now.day, sunrise_hour, sunrise_minute)
 
 		while datetime.datetime.now() < target_time:
+			print("waiting for target_time: " + str(target_time))
+			print("the current time is: " + str(datetime.datetime.now()))
 			time.sleep(60)
 
 		ctypes.windll.user32.SystemParametersInfoW(20, 0, "W:\\Backup\\Documents\\Wallpapers\\uow8tl3r8sw41.jpg", 0)
 
 		while datetime.datetime.now().hour < 12:
+			print("waiting for noon, current hour is: " + str(datetime.datetime.now().hour))
 			time.sleep(1200)
 
-		changewallpaper()
+		change_wallpaper()
 
 change_wallpaper()
