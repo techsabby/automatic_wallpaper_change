113b11545c4bc051249ea2d671cabfefbdeab8d5
techsabby <sebastian.login@proton.me>
2023-09-06 19:50:45 -0400

    Added logging

--- Linux/main.py
+++ Linux/main.py
@@ -1,19 +1,24 @@
 import time
+import logging
 import datetime
 import functions
+
+logging.basicConfig(filename='log_file.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 # this program gets the sunrise and sunset times for your area 
 # via an api call and compares it against the current time to automatically 
 # to set your wallpaper based on if its day or night time :) 
 
 while True:
+    logging.info("Program started")
     today_date = str(datetime.date.today())
-    api_data = functions.return_apicall(today_date) # returns sunrise and sunset times 
-    sunrise_date_time_EST = functions.return_sunrise_date_time_EST(api_data, datetime.date.today()) # extracts sunrise time
-    sunset_date_time_EST = functions.return_sunset_date_time_EST(api_data, datetime.date.today()) # extracts sunset time 
+    api_data = functions.return_apicall(today_date) 
+    sunrise_date_time_EST = functions.return_sunrise_date_time_EST(api_data, datetime.date.today()) 
+    sunset_date_time_EST = functions.return_sunset_date_time_EST(api_data, datetime.date.today()) 
 
     # checks to see if it is after sunset (waits for tomorrow's sunrise if it is) otherwise waits for either today's sunrise or sunset
     if (datetime.datetime.now() > sunset_date_time_EST):
+        logging.info("1")
         tomorrow_date = datetime.date.today() + datetime.timedelta(days=1)
         api_data = functions.return_apicall(today_date) # still passing today_date b/c api provides next day info after sunset
         sunrise_date_time_EST = functions.return_sunrise_date_time_EST(api_data, tomorrow_date)
@@ -24,22 +29,25 @@ while True:
             time.sleep(60)
 
         functions.run_gsettings("set_day_wallpaper")
+        logging.info("2")
 
     else:
         if (datetime.datetime.now() < sunrise_date_time_EST):
-
+            logging.info("3")
             functions.run_gsettings("set_night_wallpaper")
 
             while(datetime.datetime.now() < sunrise_date_time_EST):
                 time.sleep(60)
 
             functions.run_gsettings("set_day_wallpaper")
+            logging.info("4")
 
         else:
-
+            logging.info("5")
             functions.run_gsettings("set_day_wallpaper")
 
             while(datetime.datetime.now() < sunset_date_time_EST):
                 time.sleep(60)
 
             functions.run_gsettings("set_night_wallpaper")
+            logging.info("6")
